21517ec286295dc434c91330a397f8953d667a89
wanghaogithub720 <wanghaomspace@gmail.com>
2015-01-02 18:28:10 +0800

    add SqliteArraySortHelperTests.

---
+++ UnitTest/SqliteArraySortHelperTests.m
@@ -0,0 +1,56 @@
+//
+//  SqliteArraySortHelperTests.m
+//  OnlineVideoClient
+//
+//  Created by djzhang on 1/2/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import "SqliteArraySortHelper.h"
+
+
+@interface SqliteArraySortHelperTests : XCTestCase
+
+@end
+
+
+@implementation SqliteArraySortHelperTests
+
+- (void)setUp {
+   [super setUp];
+   // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+
+- (void)tearDown {
+   // Put teardown code here. This method is called after the invocation of each test method in the class.
+   [super tearDown];
+}
+
+
+- (void)testExample {
+   // This is an example of a functional test case.
+   NSString * str = @"stack";
+   NSMutableArray * charArray = [NSMutableArray arrayWithCapacity:str.length];
+   for (int i = 0; i < str.length; ++i) {
+      NSString * charStr = [str substringWithRange:NSMakeRange(i, 1)];
+      [charArray addObject:charStr];
+   }
+
+   [charArray sortUsingComparator:^(NSString * a, NSString * b) {
+       return [a compare:b];
+   }];
+
+   NSString * debug = @"debug";
+
+}
+
+
+- (void)testPerformanceExample {
+
+}
+
+@end
